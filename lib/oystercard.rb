78f43bf8f8ac89b84c1b1d59b4652e7eb08b289c
Corina <fluturelcorina@gmail.com>
2017-06-12 16:27:40 +0100

    Adding balance and limit to the cards

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,17 @@
 
 class Oystercard
-
+attr_reader :balance
 STARTING_BALANCE = 0
+LIMIT = 90
 
 def initialize(balance = STARTING_BALANCE)
   @balance = balance
 end
 
-def balance
-  @balance
+def top_up(amount)
+  raise "Max balance #{LIMIT} exceeded" if ((@balance + amount) > LIMIT)
+  @balance += amount
 end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,7 +11,17 @@ describe Oystercard do
     it "should have a balance when initialized" do
       expect(card.balance).to eq 0
     end
+  end
 
+  describe "#top_up" do
+    it "should add the argument to the balance" do
+      expect{ card.top_up(5) }.to change{ card.balance }.from(0).to(5)
+      end
+
+    it "raises error if max balance exceeded" do
+      over_limit = Oystercard::LIMIT + 1
+      expect { card.top_up(over_limit) }.to raise_error("Max balance #{Oystercard::LIMIT} exceeded")
+    end
   end
 
 end
