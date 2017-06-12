120d4f69e57b2f86bd5788ae6ef623147f67281f
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-12 17:18:02 +0100

    Adding touch_in etc. - also related specs

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,37 @@
 
 class Oystercard
+
 attr_reader :balance
+
+attr_accessor :in_transit
+
 STARTING_BALANCE = 0
 LIMIT = 90
 
 def initialize(balance = STARTING_BALANCE)
   @balance = balance
+  @in_transit = false
 end
 
 def top_up(amount)
   raise "Max balance #{LIMIT} exceeded" if ((@balance + amount) > LIMIT)
   @balance += amount
+end
+
+def deduct(amount)
+  @balance -= amount
+end
+
+def touch_in
+  @in_transit= true
+end
+
+def touch_out
+  @in_transit = false
+end
+
+def in_journey?
+  in_transit
 end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -24,4 +24,71 @@ describe Oystercard do
     end
   end
 
+  describe "#deduct" do
+
+    before do
+      card.top_up(10)
+    end
+
+     it "will deduct the amount passed from balance" do
+       expect { card.deduct(5) }.to change { card.balance }.from(10).to(5)
+     end
+
+  end
+
+  describe "#touch_in" do
+
+    #set up so that card has money.
+
+    it "changes @in_transit to \'true\'" do
+       expect { card.touch_in }.to change { card.in_transit}.from(false).to(true)
+    end
+  end
+
+  describe "#touch_out" do
+
+    before do
+      card.touch_in
+    end
+
+    it "changes @in_transit to \'false\'" do
+       expect { card.touch_out }.to change { card.in_transit}.from(true).to(false)
+    end
+
+
+  end
+
+  describe "#in_journey?" do
+
+    context "when user is in transit" do
+
+      before do
+        card.touch_in
+      end
+
+      it "returns true" do
+        expect(card).to be_in_journey
+      end
+
+    end
+
+    context "when user is not in transit" do
+
+      it "returns false" do
+        expect(card).not_to be_in_journey
+      end
+
+    end
+
+
+
+  end
+
+
+
+
+
+
+
+
 end
